f3718ed3f5283111cb0d7b4f311085ebb2e1d680
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-12-23 12:04:39 +0100

    Update Nuxt configuration to disable devtools, add global CSS, and integrate Tailwind CSS with PostCSS. Introduce new modules: @nuxt/icon, @nuxt/fonts, nuxt-swiper, @vueuse/nuxt, and @nuxt/image. Configure Google Fonts and set runtime configuration for API and app details. Update package dependencies accordingly.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    'nuxt-swiper',
+    '@vueuse/nuxt',
+    "@nuxt/image",
+  ],
+
+  fonts: {
+    families: [
+      { name: 'Poppins', provider: 'google' },
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: 'https://v2.afrik-flow-api.username-blakvghost.com',
+      apiUrl: 'https://v2.afrik-flow-api.username-blakvghost.com/api/v1',
+      storageUrl: 'https://v2.afrik-flow-api.username-blakvghost.com/storage',
+      appVersion: '1.0.0',
+      appUrl: 'https://afrikflow-front-end.vercel.app',
+      appEmail: 'ferraydigitalsolutions@gmail.com',
+      appPhone: '+229 0196 4311 50'
+    },
+  }
 })
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    container: {
+      padding: '2rem',
+      center: true,
+    },
+    colors: {
+      'transparent': 'transparent',
+      'primary': '#17706E',
+      'secondary': '#F6B339',
+      'light': '#eeebe6',
+      'warning': '#F6B339',
+      'red': '#892024',
+      'dark': 'rgb(42 42 41)',
+      'gray': 'rgb(150 150 150)',
+      'black': 'rgb(0 0 0)',
+      'white': 'rgb(255 255 255)',
+      'yellow': 'rgb(255 255 0)',
+      'green': 'rgb(0 255 0)',
+      'dark-transparent': 'rgba(0,0,0,.4)',
+      'dark-transparent-x': 'rgba(0,0,0,.2)',
+    },
+  },
+  plugins: [],
+}
+
